51e8df3fac405fcd9e260d3ca883cdd0dcde553f
reslava <rafeslava@hotmail.com>
2026-01-07 08:04:01 +0100

    feat: add custom error for cheking

---
+++ src/Reasons/ErrorsCustom.cs
@@ -0,0 +1,10 @@
+﻿namespace REslava.Result;
+
+public class StartDateIsAfterEndDateError : Error
+{
+    public StartDateIsAfterEndDateError (DateTime startDate, DateTime endDate)
+    {
+        base.Message = ($"The start date {startDate} is after the end date {endDate}");
+        Tags.Add ("ErrorCode", "12");
+    }
+}
